d3ebf2784bde9f75a12f0287ba92e6eaab8ff785
prince-javiya <princejaviya1234@gmail.com>
2024-10-21 17:06:50 +0530

    1. cross origin policy fixed

--- src/utils/web-socket.ts
+++ src/utils/web-socket.ts
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 export const initializeWebSocket = (server: any) => {
   const io = new Server(server, {
     cors: {
-      origin: 'http://localhost:5173',
+      origin: ['http://localhost:5173', 'https://talkwallet.netlify.app']
     },
   });
   return io;
