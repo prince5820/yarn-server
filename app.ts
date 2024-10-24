e77babd6f7a06521df3234dfea4203d9e37255ed
prince-javiya <princejaviya1234@gmail.com>
2024-10-24 13:11:08 +0530

    1. try another path for serve files

--- app.ts
+++ app.ts
@@ -39,7 +39,7 @@ app.use(contactRouter);
 app.use(chatRouter);
 chatRoutes(io);
 app.use(autoPayRouter);
-app.use('/uploads', express.static(path.join('public/uploads')));
+app.use('/uploads', express.static(path.join('/public/uploads')));
 console.log('Serving uploads from:', path.join('public/uploads'));
 
 // cron.schedule('*/5 * * * *', () => {
