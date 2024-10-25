c3403c4948de4abb247a76a7f935f921354247e6
prince-javiya <princejaviya1234@gmail.com>
2024-10-25 17:02:35 +0530

    1. change in config for other files

--- src/config/multer-cloudinary-config.ts
+++ src/config/multer-cloudinary-config.ts
@@ -4,10 +4,16 @@ import multer from "multer";
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: 'uploads',
-    resource_type: "auto"
-  } as Record<string, any>
-})
+  params: async (req: any, file: any) => {
+    const extension = file.originalname.split('.').pop().toLowerCase();
+    const isImage = file.mimetype.startsWith("image/");
+
+    return {
+      folder: 'uploads',
+      resource_type: isImage ? "image" : "raw", // Use "raw" for non-images
+      format: extension,
+    };
+  },
+});
 
 export const upload = multer({ storage });
