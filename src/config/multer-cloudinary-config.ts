e2e79fce435f63a4c470d3fe8641891307779b6f
prince-javiya <princejaviya1234@gmail.com>
2024-10-25 13:26:20 +0530

    1. change configuration for all file types

--- src/config/multer-cloudinary-config.ts
+++ src/config/multer-cloudinary-config.ts
@@ -5,7 +5,12 @@ import multer from "multer";
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'uploads'
+    folder: 'uploads',
+    resource_type: "auto",
+    format: async (req: any, file: any) => {
+      const extension = file.originalname.split(".").pop();
+      return extension; // Keeps original file extension
+    },
   } as Record<string, any>
 })
 
