a584641662d46640778406a8309847819f7651ef
prince-javiya <princejaviya1234@gmail.com>
2024-10-28 15:45:25 +0530

    1. add emoji support for render emojis in pdf

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -14,6 +14,7 @@ import { convertArrayKeysToCamelCase, convertKeysToCamelCase } from "../common/u
 import { formatDate } from "../common/utils/date-trimmer";
 import transporter from "../config/mail-config";
 import dbConnection from "../utils/db-connection";
+import path from "path";
 
 export const getUsers = (req: Request, res: Response) => {
   dbConnection.query('SELECT * FROM is_user', (err: MysqlError | null, result: User[]) => {
@@ -200,10 +201,13 @@ export const generatePdf = (req: Request, res: Response) => {
         size: 'A4',
       });
 
+      const notoEmojiPath = path.join(__dirname, '../assets/Noto_Color_Emoji/NotoColorEmoji-Regular.ttf');
+      doc.registerFont('NotoColorEmoji', notoEmojiPath);
+
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader('Content-Disposition', 'attachment');
 
-      doc.fontSize(20).text('Chat History', { align: 'center', underline: true });
+      doc.font('NotoColorEmoji').fontSize(20).text('Chat History', { align: 'center', underline: true });
       doc.moveDown(2); // Add space below the title
 
       doc.fontSize(14).text(`Username: ${userName}`, { align: 'left' });
@@ -254,13 +258,11 @@ export const generatePdf = (req: Request, res: Response) => {
           .fillColor('black');
 
         // Draw the message text
-        doc
-          .fontSize(messageFontSize)
-          .text(message_text, textPositionX + 10, doc.y + 10, {
-            width: textBoxWidth - 20,
-            align: alignment,
-          })
-          .moveDown(2); // Add space after each message
+        doc.font('NotoColorEmoji').fontSize(14).text(message_text, textPositionX + 10, doc.y + 10, {
+          width: textBoxWidth - 20,
+          align: alignment,
+        });
+        doc.moveDown(2);
       });
 
       // Finalize the PDF and stream it in response
