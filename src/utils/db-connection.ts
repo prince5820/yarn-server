cc3353f3e38de8b3703e6bddff6464fec8e9dbb1
prince-javiya <princejaviya1234@gmail.com>
2024-10-28 11:48:46 +0530

    1. fix fatal error of database connection

--- src/utils/db-connection.ts
+++ src/utils/db-connection.ts
@@ -1,33 +1,12 @@
-import mysql, { Connection } from "mysql";
+import mysql, { Pool } from "mysql";
 
-let dbConnection: Connection | null = null;
+const dbConnection: Pool = mysql.createPool({
+  connectionLimit: 10, // Number of connections in the pool
+  host: 'bj4pcstpt5emmq5btlg8-mysql.services.clever-cloud.com',
+  user: 'uguehyt6jxhmyfrd',
+  password: '87Adyy1OtVcS6KsTBcdm',
+  database: 'bj4pcstpt5emmq5btlg8',
+  charset: 'utf8mb4'
+});
 
-const handleDisconnect = () => {
-  dbConnection = mysql.createConnection({
-    host: 'bj4pcstpt5emmq5btlg8-mysql.services.clever-cloud.com',
-    user: 'uguehyt6jxhmyfrd',
-    password: '87Adyy1OtVcS6KsTBcdm',
-    database: 'bj4pcstpt5emmq5btlg8',
-    charset: 'utf8mb4'
-  });
-
-  dbConnection.connect(err => {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
-    }
-  });
-
-  // Use a non-null assertion to assure TypeScript that dbConnection is assigned
-  dbConnection.on('error', err => {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect(); // Reconnect on connection loss
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
-
-export default dbConnection!;
+export default dbConnection;
