0af4ee85f77bdea5cb5488f0d1cfe6524d470574
prince-javiya <princejaviya1234@gmail.com>
2024-10-28 11:19:12 +0530

    1. fix unhandled error

--- src/utils/db-connection.ts
+++ src/utils/db-connection.ts
@@ -1,11 +1,33 @@
 import mysql, { Connection } from "mysql";
 
-const dbConnection: Connection = mysql.createConnection({
-  host: 'bj4pcstpt5emmq5btlg8-mysql.services.clever-cloud.com',
-  user: 'uguehyt6jxhmyfrd',
-  password: '87Adyy1OtVcS6KsTBcdm',
-  database: 'bj4pcstpt5emmq5btlg8',
-  charset: 'utf8mb4'
-});
+let dbConnection: Connection | null = null;
 
-export default dbConnection;
+const handleDisconnect = () => {
+  dbConnection = mysql.createConnection({
+    host: 'bj4pcstpt5emmq5btlg8-mysql.services.clever-cloud.com',
+    user: 'uguehyt6jxhmyfrd',
+    password: '87Adyy1OtVcS6KsTBcdm',
+    database: 'bj4pcstpt5emmq5btlg8',
+    charset: 'utf8mb4'
+  });
+
+  dbConnection.connect(err => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+    }
+  });
+
+  // Use a non-null assertion to assure TypeScript that dbConnection is assigned
+  dbConnection.on('error', err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconnect on connection loss
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+export default dbConnection!;
