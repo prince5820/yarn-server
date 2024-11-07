ffc598d587250d80bf356b842ce901d1a4ff2185
prince-javiya <princejaviya1234@gmail.com>
2024-11-07 16:02:25 +0530

    1. fix error in send message while values entry in database

--- src/controllers/chat-controller.ts
+++ src/controllers/chat-controller.ts
@@ -83,7 +83,7 @@ export const sendMessage = (req: Request, res: Response, io: any) => {
       const fileName = file.originalname;
       const fileType = file.mimetype;
       const filePath = file.path;
-      const fileSql = 'INSERT INTO chat (message_text, sender_id, receiver_id, is_read, file_name, file_type, file_path) VALUES (?, ?, ?, ?, ?, ?)';
+      const fileSql = 'INSERT INTO chat (message_text, sender_id, receiver_id, is_read, file_name, file_type, file_path) VALUES (?, ?, ?, ?, ?, ?, ?)';
       const fileValues = [null, senderId, receiverId, 0, fileName, fileType, filePath];
 
       dbConnection.query(fileSql, fileValues, (err: MysqlError | null, result: MysqlResult) => {
@@ -114,7 +114,7 @@ export const sendMessage = (req: Request, res: Response, io: any) => {
       });
     } else {
       if (messageText) {
-        const fileSql = 'INSERT INTO chat (message_text, sender_id, receiver_id, is_read, file_name, file_type, file_path) VALUES (?, ?, ?, ?, ?, ?)';
+        const fileSql = 'INSERT INTO chat (message_text, sender_id, receiver_id, is_read, file_name, file_type, file_path) VALUES (?, ?, ?, ?, ?, ?, ?)';
         const fileValues = [messageText, senderId, receiverId, 0, null, null, null];
 
         dbConnection.query(fileSql, fileValues, (err: MysqlError | null, result: MysqlResult) => {
